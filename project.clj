c20db0233e0b4093ceae729df38c3b1399850687
Carl Hu <carlhu@carlhu.com>
2015-01-11 13:59:41 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url                "https://github.com/thebusby/iota"
   :license            {:name "MIT"
                        :url "http://opensource.org/licenses/MIT"}
-  :dependencies       [[org.clojure/clojure "1.5.1"]
+  :dependencies       [[org.clojure/clojure "1.6"]
                        [org.codehaus.jsr166-mirror/jsr166y "1.7.0"] ;; To support reducers on Java 1.6
                        ]
   :source-paths       ["src/clj"]
